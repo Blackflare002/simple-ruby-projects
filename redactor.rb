a60c5e91f604b2233eba129742c104c8ed75b52d
Shane Phillips <shane@loongphillips.com>
2022-09-02 15:18:06 -0400

    implement the word redactor project

--- redactor.rb
+++ redactor.rb
@@ -1,1 +1,11 @@
-# nothing
+puts "Input a list of words: "
+words = gets.chomp
+puts "Input ONE word you'd like censored: "
+censor = gets.chomp
+list = words.split(" ")
+list.each do |e|
+    if e == censor
+        print "***** "
+    else print e + " "
+    end
+end
